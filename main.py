883081f31121bb486d43a46b56ef889e734982d3
Ryan O'Mullan <omullan.ryan@gmail.com>
2016-01-22 17:56:01 -0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import urllib
+from BeautifulSoup import *
+
+def main():
+    
+    url = raw_input('Enter URL - ')
+    
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
+    
+    tags = soup('span')
+    total = 0
+    
+    for tag in tags:
+        total += int(tag.contents[0])
+    
+    print('Count %s' % len(tags))
+    print('Sum %s' % total)
+    
+    
+if __name__ == "__main__":
+    main()
